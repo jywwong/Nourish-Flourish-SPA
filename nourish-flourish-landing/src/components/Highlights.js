2315cd1ee9a7581947ee0895c15b0bb69d6c348f
jyww <yourname@example.com119052363+jywwong@users.noreply.github.com>
2023-06-15 16:59:53 +0100

    implemented highlights scroll function

--- nourish-flourish-landing/src/components/Highlights.js
+++ nourish-flourish-landing/src/components/Highlights.js
@@ -3,21 +3,29 @@ import {IoIosArrowDown} from "react-icons/io";
 import Juice from './assets/Green_goddess_juice.jpg';
 import Bowl from './assets/eat-the-rainbow-bowl.jpg';
 import Cookies from './assets/cookies.jpg';
+import React, {useRef} from 'react';
 
-function Highlights(){
-return(
+const Highlights = () => {
+    const scrollRef = useRef(null);
+    const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({behavior:'smooth'});
+    }
+};
+return (
 <div className="main-container">
 <div className="juice_container">
     <img className="greengoddessimg" src={Juice} alt="Green Goddess Juice" />
-    <button className="btn"><IoIosArrowDown /></button>
+    <button className="btn" onClick={() => scrollToSection('menu')}><IoIosArrowDown /></button>
 </div>
 <div className="bowl_container">
 <img className="bowlimg" src={Bowl} alt="Bowl" />
-<button className="btn"><IoIosArrowDown /></button>
+<button className="btn" onClick={() => scrollToSection('menu')}><IoIosArrowDown /></button>
 </div>
 <div className="cookies_container">
 <img className="vegancookies" src={Cookies} alt="Vegan Cookies" />
-<button className="btn"><IoIosArrowDown /></button>
+<button className="btn" onClick={() => scrollToSection('menu')}><IoIosArrowDown /></button>
 </div></div>
 );
 }
